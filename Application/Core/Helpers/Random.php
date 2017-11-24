c5530ddafedbd067ab71bac8e35f0980581e0c3f
Ujjwal Bhardwaj <ujjwalb1996@gmail.com>
2017-11-24 23:50:14 +0530

    Create and check password reset token

---
+++ Application/Core/Helpers/Random.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Brime\Core\Helpers;
+
+class Random
+{
+    public function __construct() { }
+
+    public function generateString($length, $characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/')
+    {
+        $maxCharIndex = strlen($characters) - 1;
+        $randomString = '';
+
+        while ($length > 0) {
+            $randomNumber = \random_int(0, $maxCharIndex);
+            $randomString = $characters[$randomNumber];
+            $length--;
+        }
+
+        return $randomString;
+    }
+
+}
